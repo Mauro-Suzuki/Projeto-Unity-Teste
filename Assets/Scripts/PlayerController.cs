ba7f5d558ae80f0887bb921024d70e4b3ef6368b
Mauro Suzuki <maurosuzuki92@gmail.com>
2022-02-15 14:13:59 -0300

    movendo comando getcomponent para fora do update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float _velocity;
-
+    private Rigidbody2D rbody;
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }//comando getcomponent movido do update para o start
     void Update()
     {
-        var rbody = GetComponent<Rigidbody2D>();
-
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             rbody.velocity = Vector2.left * _velocity;
